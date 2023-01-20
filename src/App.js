a57a54af9a934e4c901cf55a059a998cd0cdf33f
Keerthana-Singh <keerthanasadhusundarsingh@gmail.com>
2023-01-20 11:59:52 +0530

    Dashboard Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import { Login } from './Components/Login/Login';
+import { Dashboard } from './Components/Dashboard/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <Dashboard/>
     </div>
   );
 }
