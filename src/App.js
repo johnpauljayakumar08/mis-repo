af14b54858d125925bf86403d87da2c5a59d92c4
johnstarberry <johnpaul@starberry.tv>
2023-01-22 10:33:24 +0530

    menu

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import { Login } from './Components/Login/Login';
 import { Dashboard } from './Components/Dashboard/Dashboard';
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 function App() {
   return (
-    <div className="App">
-      <Dashboard/>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login/>}/>
+          <Route path='/Dashboard' element={<Dashboard/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,63 +1,60 @@
 import React from "react";
 import './Dashboard.css'
-
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 export function Dashboard(){
-            document.addEventListener("DOMContentLoaded", function(event) {
-        
-                const showNavbar = (toggleId, navId, bodyId, headerId) =>{
-                const toggle = document.getElementById(toggleId),
-                nav = document.getElementById(navId),
-                bodypd = document.getElementById(bodyId),
-                headerpd = document.getElementById(headerId)
-                
-                // Validate that all variables exist
-                if(toggle && nav && bodypd && headerpd){
-                toggle.addEventListener('click', ()=>{
-                // show navbar
-                nav.classList.toggle('show')
-                // change icon
-                toggle.classList.toggle('bx-x')
-                // add padding to body
-                bodypd.classList.toggle('body-pd')
-                // add padding to header
-                headerpd.classList.toggle('body-pd')
-                })
-                }
-                }
-                
-                showNavbar('header-toggle','nav-bar','body-pd','header')
-                
-                /*===== LINK ACTIVE =====*/
-                const linkColor = document.querySelectorAll('.nav_link')
-                
-                function colorLink(){
-                if(linkColor){
-                linkColor.forEach(l=> l.classList.remove('active'))
-                this.classList.add('active')
-                }
-                }
-                linkColor.forEach(l=> l.addEventListener('click', colorLink))
-                
-                // Your code to run since DOM is loaded and ready
-                });
+      
      return(
         <>
-            <body id="body-pd">
-            <header class="header" id="header">
-                <div class="header_toggle"> <i class='bx bx-menu' id="header-toggle"></i> </div>
-                <div class="header_img"> <img src="https://i.imgur.com/hczKIze.jpg" alt=""/> </div>
-            </header>
-            <div class="l-navbar" id="nav-bar">
-                <nav class="nav">
-                    <div> <a href="#" class="nav_logo"> <i class='bx bx-layer nav_logo-icon'></i> <span class="nav_logo-name">BBBootstrap</span> </a>
-                        <div class="nav_list"> <a href="#" class="nav_link active"> <i class='bx bx-grid-alt nav_icon'></i> <span class="nav_name">Dashboard</span> </a> <a href="#" class="nav_link"> <i class='bx bx-user nav_icon'></i> <span class="nav_name">Users</span> </a> <a href="#" class="nav_link"> <i class='bx bx-message-square-detail nav_icon'></i> <span class="nav_name">Messages</span> </a> <a href="#" class="nav_link"> <i class='bx bx-bookmark nav_icon'></i> <span class="nav_name">Bookmark</span> </a> <a href="#" class="nav_link"> <i class='bx bx-folder nav_icon'></i> <span class="nav_name">Files</span> </a> <a href="#" class="nav_link"> <i class='bx bx-bar-chart-alt-2 nav_icon'></i> <span class="nav_name">Stats</span> </a> </div>
-                    </div> <a href="#" class="nav_link"> <i class='bx bx-log-out nav_icon'></i> <span class="nav_name">SignOut</span> </a>
-                </nav>
-            </div>
-            <div class="height-100 bg-light">
-                <h4>Main Components</h4>
-            </div>
-            </body>
+           <nav class="navbar bg-body-tertiary fixed-top">
+                <div class="container-fluid">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <a class="navbar-brand" href="#"><img src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp" class="rounded-circle" style={{height:"70px"}}
+                    alt="Avatar" /></a>
+                    <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                    <div class="offcanvas-header">
+                        <h5 class="offcanvas-title" id="offcanvasNavbarLabel"></h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </div>
+                    <div class="offcanvas-body">
+                        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Dashboard</a>
+                        </li>
+                        {/* <li class="nav-item">
+                            <a class="nav-link" href="#"></a>
+                        </li> */}
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"  aria-expanded="false">
+                            Student
+                            </a>
+                            <ul class="dropdown-menu" id="menu">
+                            <li className="nav-item">
+                                <a class="nav-link " href="#">Software Programing</a>
+                               
+                            </li>
+                            <li className="nav-item">
+                                <a class="nav-link" href="#">IMS</a>
+                            </li>
+                            
+                            <li className="nav-item">
+                                <a class="nav-link " href="#" >SAP</a>
+                            </li>
+                            </ul>
+                            
+                        </li>
+                        </ul>
+                        <form class="d-flex mt-3" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                    </div>
+                    </div>
+                </div>
+            </nav> 
+             
         </>
      );
 }
