58d0be7d0d03743d9e9fe425ab8389d4f7a901b1
Keerthana-Singh <johnpaul@starberry.tv>
2023-01-22 11:49:17 +0530

    chart

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import { Login } from './Components/Login/Login';
 import { Dashboard } from './Components/Dashboard/Dashboard';
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import { Chart } from './Components/chart/chart';
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Login/>}/>
+          <Route path='/' element={<Chart/>}/>
           <Route path='/Dashboard' element={<Dashboard/>}/>
         </Routes>
       </BrowserRouter>
